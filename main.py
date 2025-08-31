fae8a374ae7d17bd62911f64eee544d67bc6107b
avr2002 <avr13405@gmail.com>
2025-08-31 11:13:53 +0530

    feat: refactored the line reader to read from file and a TCP connection

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import io
 import logging
-import mmap
-import os
+import socket
 import sys
-from typing import Generator
+from typing import Callable, Generator, Optional, Union
 
 formatter = logging.Formatter(
     # fmt="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(name)s:%(funcName)s | %(message)s",
@@ -16,16 +16,43 @@ logger = logging.getLogger(name=__name__)
 logger.addHandler(handler)
 
 
-def read_lines(fpath: str) -> Generator[str, None, None]:
-    """Stream a text file line-by-line using raw OS syscalls (`os.open`/`os.read`) with low allocation overhead."""
-    fd = None
+def get_lines_from_reader(stream: Union[io.FileIO, socket.SocketIO, socket.socket]) -> Generator[str, None, None]:
+    r"""
+    Incrementally read text lines from a binary stream or socket.
+
+    The function consumes bytes from the given stream-like object in
+    small chunks, accumulates them in a buffer, and yields decoded
+    lines one at a time when a newline character is encountered.
+
+    :param reader: io.FileIO or socket.SocketIO or socket.socket
+        A file-like object with a ``.read(size) -> bytes`` method, or
+        a socket-like object with a ``.recv(size) -> bytes`` method.
+
+    :yield: str
+        Decoded lines of text without trailing newline characters.
+
+    """
+    # if not (isinstance(f, io.FileIO) or isinstance(f, socket.SocketIO) or isinstance(f, socket.socket)):
+    #     raise ValueError("Invalid file or connection type provided!")
+
+    reader: Optional[Callable[[int], bytes]] = getattr(stream, "read", None)
+    receiver: Optional[Callable[[int], bytes]] = getattr(stream, "recv", None)
+    # If f has a .read(), reader is a bound method (callable: reader(n) -> bytes).
+    # If not, it’s None. Same for receiver.
+    # So their types are “either a callable that returns bytes, or None.
+
+    if not reader and not receiver:
+        raise ValueError("Need .read() ot .recv()")
+
     try:
-        fd = os.open(fpath, flags=os.O_RDONLY)
         buffer = bytearray()
         while True:
             # Read 8 bytes at a time
-            chunk = os.read(fd, 8)
+            # chunk = f.read(8) if isinstance(f, io.FileIO) else f.recv(8)
+            chunk = reader(8) if reader else receiver(8)
             if not chunk:  # EOF reached
+                # Files: .read() returns b'' at EOF
+                # TCP: .recv() returns b'' when the peer cleanly closes
                 break
 
             buffer.extend(chunk)
@@ -39,7 +66,6 @@ def read_lines(fpath: str) -> Generator[str, None, None]:
                 if line_bytes.endswith(b"\r"):
                     line_bytes = line_bytes[:-1]
 
-                # print("read:", line_bytes.decode())
                 yield line_bytes.decode()
 
                 # Drop emitted line + '\n' from the accumulator
@@ -49,23 +75,51 @@ def read_lines(fpath: str) -> Generator[str, None, None]:
         if buffer:
             if buffer.endswith((b"\r")):
                 buffer = buffer[:-1]
-            # print("read:", buffer.decode())
             yield buffer.decode()
 
+    except Exception as e:
+        logger.exception(e)
+
+
+def get_from_file(fpath: str):
+    try:
+        with open(file=fpath, mode="rb", buffering=0) as f:
+            f: io.FileIO
+            for line in get_lines_from_reader(stream=f):
+                print("read:", line)
     except FileNotFoundError:
         logger.error("File Not Found at path: {}".format(fpath))
         sys.exit(0)
-    except Exception as e:
-        logger.exception(e)
-    finally:
-        if fd:
-            os.close(fd)
+
+
+# https://realpython.com/python-sockets/
+# https://docs.python.org/3/library/socket.html
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 42069  # Port to listen on (non-privileged ports are > 1023)
+
+
+def get_from_conn():
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()  # <-- BLOCKS until a client connects
+        # with conn:
+        #     logger.info(f"Connected by {addr=}")
+        #     for line in get_lines_from_reader(stream=conn):
+        #         print("read:", line)
+
+        # conn.makefile supports IO operations like `f.read()` instead of conn.read()^^^ like above
+        with conn.makefile("rb", buffering=0) as f:
+            logger.info(f"Connected by {addr=}")
+            for line in get_lines_from_reader(stream=f):
+                print("read:", line)
 
 
 def main():
-    path = os.curdir + "/messages.txt"
-    for line in read_lines(fpath=path):
-        print("read:", line, end="\n")
+    # path = os.curdir + "/messages.txt"
+    # get_from_file(fpath=path)
+    get_from_conn()
 
 
 if __name__ == "__main__":
