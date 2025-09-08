ab825c33d4cf4c5f3acac586998a68d9285d0e94
avr2002 <avr13405@gmail.com>
2025-09-08 17:55:56 +0530

    refactor: moved logging setup in __init__.py; update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import io
-import logging
+import os
 import socket
 import sys
 from typing import Callable, Generator, Optional, Union
 
-formatter = logging.Formatter(
-    # fmt="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(name)s:%(funcName)s | %(message)s",
-    fmt="%(levelname)s: %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%SZ",
-)
-handler = logging.StreamHandler()
-handler.setFormatter(fmt=formatter)
-handler.setLevel(level=logging.INFO)
-logger = logging.getLogger(name=__name__)
-logger.addHandler(handler)
+from tcp_to_http import logger
 
 
 def get_lines_from_reader(stream: Union[io.FileIO, socket.SocketIO, socket.socket]) -> Generator[str, None, None]:
@@ -37,12 +28,13 @@ def get_lines_from_reader(stream: Union[io.FileIO, socket.SocketIO, socket.socke
 
     reader: Optional[Callable[[int], bytes]] = getattr(stream, "read", None)
     receiver: Optional[Callable[[int], bytes]] = getattr(stream, "recv", None)
-    # If f has a .read(), reader is a bound method (callable: reader(n) -> bytes).
-    # If not, it’s None. Same for receiver.
+    # If f has a .read(), reader is a bound method (callable: reader(n) -> bytes). If not, it’s None.
+    # Similarly, the TCP connection object exposes `conn.recv()` method to read the bytes sent
+    # if you have passed the connection object as the stream. (See `get_from_conn()` method for more)
     # So their types are “either a callable that returns bytes, or None.
 
     if not reader and not receiver:
-        raise ValueError("Need .read() ot .recv()")
+        raise ValueError("Need .read() or .recv()")
 
     try:
         buffer = bytearray()
@@ -58,48 +50,61 @@ def get_lines_from_reader(stream: Union[io.FileIO, socket.SocketIO, socket.socke
             buffer.extend(chunk)
             # Emit as many complete lines in buffer
             while True:
-                nl = buffer.find(b"\n")  # new-line
-                if nl == -1:
+                new_line = buffer.find(b"\n")
+                if new_line == -1:  # Returns -1 if it cannot find the character in the buffer
                     break
 
-                line_bytes = buffer[:nl]
+                line_bytes = buffer[:new_line]
+                # Strip out any carriage return character
                 if line_bytes.endswith(b"\r"):
                     line_bytes = line_bytes[:-1]
 
                 yield line_bytes.decode()
 
                 # Drop emitted line + '\n' from the accumulator
-                del buffer[: nl + 1]
+                del buffer[: new_line + 1]
 
         # Read the last line if present
         if buffer:
             if buffer.endswith((b"\r")):
                 buffer = buffer[:-1]
             yield buffer.decode()
-
+        return
     except Exception as e:
         logger.exception(e)
 
 
-def get_from_file(fpath: str):
+def read_data_from_file(file_path: str):
+    """Read data line by line from a file."""
     try:
-        with open(file=fpath, mode="rb", buffering=0) as f:
+        with open(file=file_path, mode="rb", buffering=0) as f:
             f: io.FileIO
             for line in get_lines_from_reader(stream=f):
                 print("read:", line)
     except FileNotFoundError:
-        logger.error("File Not Found at path: {}".format(fpath))
+        logger.error("File Not Found at path: {}".format(file_path))
         sys.exit(0)
 
 
+# Socket Programming:
 # https://realpython.com/python-sockets/
+# https://youtu.be/3QiPPX-KeSc?si=W92KcIUbOqSChcLi
 # https://docs.python.org/3/library/socket.html
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 42069  # Port to listen on (non-privileged ports are > 1023)
 
 
-def get_from_conn():
+def receive_data_from_tcp_conn():
+    """
+    Receive data line by line from a TCP Connection.
+
+    How to run:
+
+    1. Run `uv run main.py`
+    2. In another terminal run `cat messages.txt | nc -w 1 127.0.0.1 42069`
+    3. If the it ran successfully, the terminal running the python script should have received the data.
+    """
     with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
@@ -109,17 +114,19 @@ def get_from_conn():
         #     for line in get_lines_from_reader(stream=conn):
         #         print("read:", line)
 
-        # conn.makefile supports IO operations like `f.read()` instead of conn.read()^^^ like above
-        with conn.makefile("rb", buffering=0) as f:
+        # conn.makefile() supports IO operations like `f.read()` instead of conn.recv()^^^ like above
+        with conn.makefile(mode="rb", buffering=0) as raw_bytes:
             logger.info(f"Connected by {addr=}")
-            for line in get_lines_from_reader(stream=f):
+            for line in get_lines_from_reader(stream=raw_bytes):
                 print("read:", line)
+
 
 
 def main():
     # path = os.curdir + "/messages.txt"
-    # get_from_file(fpath=path)
-    get_from_conn()
+    # read_data_from_file(file_path=path)
+
+    receive_data_from_tcp_conn()
 
 
 if __name__ == "__main__":
--- src/tcp_to_http/__init__.py
+++ src/tcp_to_http/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from tcp-to-http!")
+import logging
+
+formatter = logging.Formatter(
+    # fmt="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(name)s:%(funcName)s | %(message)s",
+    fmt="%(levelname)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+)
+handler = logging.StreamHandler()
+handler.setFormatter(fmt=formatter)
+handler.setLevel(level=logging.INFO)
+logger = logging.getLogger(name=__name__)
+logger.addHandler(handler)
